1f11e458602efe274538b77fa6367d4d68b5844c
Emerson Havener <emerson@nevada.unr.edu>
2023-02-19 14:12:22 -0800

    add name collection to GuessingGame

--- unit1/src/GuessingGame.java
+++ unit1/src/GuessingGame.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class GuessingGame {
     public GuessingGame() {
+
+
+    }
+
+    public static void main(String[] args){
         System.out.println("Welcome to Guessing Game!");
+        System.out.println("Enter your name:");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hello " + name + "!");
     }
 }
