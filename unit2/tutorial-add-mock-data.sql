8ec527d698bef058501eb404dbb89bd993af0d70
Emerson Havener <emerson@nevada.unr.edu>
2023-02-27 13:10:15 -0800

    add sql script for database mock data

---
+++ unit2/tutorial-add-mock-data.sql
@@ -0,0 +1,13 @@
+INSERT INTO customers VALUES ('John', 'Doe', 'jd@example.com', 5551002000, 1);
+INSERT INTO customers VALUES ('Jane', 'Doe', 'jane@example.com', 5553004000, 2);
+SELECT * FROM customers;
+
+INSERT INTO products VALUES (1, 'Trek', 'Stumpjumper', 'A mountain bike with large wheels.', '899.99', 2);
+INSERT INTO products VALUES (2, 'Gravity', 'FSX', 'A basic mountain bike.', '648.00', 4);
+SELECT * FROM products;
+
+INSERT INTO transactions VALUES ('2023-02-27', 1, 1, 1);
+INSERT INTO transactions VALUES ('2023-02-27', 2, 1, 2);
+INSERT INTO transactions VALUES ('2023-02-27', 3, 2, 1);
+SELECT * FROM transactions;
+
