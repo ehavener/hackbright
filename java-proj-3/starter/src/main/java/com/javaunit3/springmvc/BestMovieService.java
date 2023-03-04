823dad24e1d013a55a09274a6e8ebf5b2f13a919
Emerson Havener <emerson@nevada.unr.edu>
2023-03-03 19:45:41 -0800

    complete unit 3 part 2: websites

--- java-proj-3/starter/src/main/java/com/javaunit3/springmvc/BestMovieService.java
+++ java-proj-3/starter/src/main/java/com/javaunit3/springmvc/BestMovieService.java
@@ -1,13 +1,20 @@
 package com.javaunit3.springmvc;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BestMovieService {
+    @Autowired
+    public BestMovieService(@Qualifier("batmanMovie") Movie movie) {
+        this.movie = movie;
+    }
+
     private Movie movie;
 
     public Movie getBestMovie() {
+
         return movie;
     }
 }
--- java-proj-3/starter/src/main/java/com/javaunit3/springmvc/MovieController.java
+++ java-proj-3/starter/src/main/java/com/javaunit3/springmvc/MovieController.java
@@ -1,11 +1,47 @@
 package com.javaunit3.springmvc;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
 public class MovieController {
+
+    private final BestMovieService bestMovieService;
+
+    @Autowired
+    public MovieController(
+            BestMovieService bestMovieService
+    ) {
+        this.bestMovieService = bestMovieService;
+    }
+    
     @RequestMapping("/")
     public String getIndexPage()
     {
         return "index";
+    }
+
+    @RequestMapping("/bestMovie")
+    public String getBestMoviePage(Model model) {
+        model.addAttribute("BestMovie", bestMovieService.getBestMovie().getTitle());
+        return "bestMovie";
+    }
+
+    @RequestMapping("/voteForBestMovieForm")
+    public String voteForBestMovieFormPage() {
+        return "voteForBestMovie";
+    }
+
+    @RequestMapping("/voteForBestMovie")
+    public String voteForBestMovie(HttpServletRequest request, Model model) {
+        String movieTitle = request.getParameter("movieTitle");
+        model.addAttribute("BestMovieVote", movieTitle);
+        return "voteForBestMovie";
     }
 }
