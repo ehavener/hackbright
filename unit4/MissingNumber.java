08c47500d3850f2992309ab2d1bf13d103f41cd1
Emerson Havener <emerson@nevada.unr.edu>
2023-03-28 09:41:46 -0700

    implement parts 1 and 2 of java unit 5

--- unit4/MissingNumber.java
+++ unit4/MissingNumber.java
@@ -1,10 +1,12 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Set;
+import java.util.Stack;
 import java.util.stream.Collectors;
 
 public class MissingNumber {
-    
+
+    // Part 1: Runtime
     public static int missingNumber(int[] numbers, int maxNum) {
         Arrays.sort(numbers);
         for (int i = 1; i <= maxNum; i++) {
@@ -35,7 +37,28 @@ public class MissingNumber {
         return expectedSum - actualSum;
     }
 
+    // Part 2: Data Structures
+    public static boolean balancedParenthesis(String input) {
+        Stack<Character> s = new Stack<Character>();
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == ')') {
+                if (!s.empty() && s.peek() == '(') {
+                    s.pop();
+                } else {
+                    return false;
+                }
+            } else if (input.charAt(i) == '(') {
+                s.add('(');
+            }
+        }
+
+        return s.isEmpty() ? true : false;
+    }
+
+    // Part 3: Data Structures and Algorithms Practice
+
     public static void main(String[] args) {
+        // Part 1: Runtime
         int[] numbers = {2, 1, 4, 3, 6, 5, 7, 10, 9};
         int maxNum = 10;
         int result = missingNumber(numbers, maxNum);
@@ -50,5 +73,13 @@ public class MissingNumber {
         int maxNumOnO1 = 10;
         int resultOnO1 = missingNumberOnO1(numbersOnO1, maxNumOnO1);
         System.out.println(resultOnO1);
+
+        // Part 2: Data Structures
+        String input = "(()))";
+        System.out.println(balancedParenthesis(input));
+
+
+        // Part 3: Data Structures and Algorithms Practice
+        
     }
 }
